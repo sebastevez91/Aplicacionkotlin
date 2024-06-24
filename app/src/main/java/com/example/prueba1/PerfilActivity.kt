2067092d37b74ed7ab6f4803093e5d78e6a25f7f
Sebastian <sebastevez91@gmail.com>
2024-06-24 17:58:39 -0300

    Actualizo PerfilActivity.kt y activity_perfil.xml

--- app/src/main/java/com/example/prueba1/PerfilActivity.kt
+++ app/src/main/java/com/example/prueba1/PerfilActivity.kt
@@ -1,5 +1,6 @@
 package com.example.prueba1
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.DatePicker
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isEmpty
+import java.util.Calendar
 
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +27,14 @@ class PerfilActivity : AppCompatActivity() {
         val nombre = findViewById<EditText>(R.id.Nombre)
         val apellido = findViewById<EditText>(R.id.Apellido)
         val fechaNac = findViewById<DatePicker>(R.id.FechaNacimiento)
-        val  tipoDni = findViewById<Spinner>(R.id.TipoDni)
+        val tipoDni = findViewById<Spinner>(R.id.TipoDni)
         val numDni = findViewById<EditText>(R.id.numDni)
         val btnGuardar = findViewById<Button>(R.id.BtnGuardar)
+        val etDate = findViewById<EditText>(R.id.etDate)
 
+        etDate.setOnClickListener {
+            showDateNacimiento()
+        }
         btnGuardar.setOnClickListener {
 
         }
@@ -38,6 +44,23 @@ class PerfilActivity : AppCompatActivity() {
             showToast("Debes completar los campos.")
         }else if ()
     }*/
+    private fun showDateNacimiento(){
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePicker = DatePickerDialog(
+            this,{ _, selectedYear, selectedMonth, selectedDay ->
+                val formattedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                etDate.setText(formattedDate)
+            },
+            year,
+            month,
+            day
+        )
+        datePicker.show()
+    }
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
